19ba9015589bb10ad5aec6f0acbfe87195faec0d
aRooj-saghaR <aroojsaghar648@gmail.com>
2022-06-04 02:49:22 -0700

    updated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+$('.nav-trigger').click(function() {
+    $('.site-content-wrapper').toggleClass('scaled');
+})
+
+
 $('.testi.owl-carousel').owlCarousel({
     items: 2,
     margin: 10,
@@ -92,4 +97,33 @@ function openTabContentOurServices(evt, containerToDisplay, className, that, tab
 document.getElementById("defaultOpen").click();
 $(".compare_prices").click(function() {
     $('#compareModal').modal('toggle');
+})
+
+
+var owl = $('.owl-1');
+owl.owlCarousel({
+    loop: false,
+    margin: 0,
+    nav: false,
+    dots: false,
+    items: 1,
+    smartSpeed: 1000,
+    autoplay: false,
+    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+});
+
+var carousel_nav_a = $('.carousel-nav a');
+
+carousel_nav_a.each(function(slide_index) {
+    var $this = $(this);
+    $this.attr('data-num', slide_index);
+    $this.click(function(e) {
+        owl.trigger('to.owl.carousel', [slide_index, 1500]);
+        e.preventDefault();
+    })
+})
+
+owl.on('changed.owl.carousel', function(event) {
+    carousel_nav_a.removeClass('active');
+    $(".carousel-nav a[data-num=" + event.item.index + "]").addClass('active');
 })
