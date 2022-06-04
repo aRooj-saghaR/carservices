1c66a2d549c6b899f6ece9947a4ad27005b40c16
aRooj-saghaR <aroojsaghar648@gmail.com>
2022-06-04 03:35:50 -0700

    updated

--- js/app.js
+++ js/app.js
@@ -99,31 +99,34 @@ $(".compare_prices").click(function() {
     $('#compareModal').modal('toggle');
 })
 
+$(document).ready(function() {
+    var owl = $('.owl-1');
+    owl.owlCarousel({
+        loop: false,
+        margin: 0,
+        nav: false,
+        dots: false,
+        items: 1,
+        smartSpeed: 1000,
+        autoplay: false,
+        navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+    });
 
-var owl = $('.owl-1');
-owl.owlCarousel({
-    loop: false,
-    margin: 0,
-    nav: false,
-    dots: false,
-    items: 1,
-    smartSpeed: 1000,
-    autoplay: false,
-    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
-});
+    var carousel_nav_a = $('.carousel-nav a');
 
-var carousel_nav_a = $('.carousel-nav a');
-
-carousel_nav_a.each(function(slide_index) {
-    var $this = $(this);
-    $this.attr('data-num', slide_index);
-    $this.click(function(e) {
-        owl.trigger('to.owl.carousel', [slide_index, 1500]);
-        e.preventDefault();
+    carousel_nav_a.each(function(slide_index) {
+        var $this = $(this);
+        $this.attr('data-num', slide_index);
+        $this.click(function(e) {
+            owl.trigger('to.owl.carousel', [slide_index, 1500]);
+            e.preventDefault();
+        })
     })
-})
 
-owl.on('changed.owl.carousel', function(event) {
-    carousel_nav_a.removeClass('active');
-    $(".carousel-nav a[data-num=" + event.item.index + "]").addClass('active');
-})
+    owl.on('changed.owl.carousel', function(event) {
+        carousel_nav_a.removeClass('active');
+        $(".carousel-nav a[data-num=" + event.item.index + "]").addClass('active');
+    })
+
+
+});
